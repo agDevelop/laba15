dde696dfcf330e3b98fa4046a67418f20f33dc6e
agDevelop <andrewnikolaevich.2014@gmail.com>
2019-12-12 21:55:44 +0300

    ready
    ready

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {MainComponent} from "./main/main.component";
+import {PersonsListComponent} from "./persons-list/persons-list.component";
+import {PersonAddComponent} from "./person-add/person-add.component";
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: MainComponent},
+  {path: 'list', component: PersonsListComponent},
+  {path: 'add', component: PersonAddComponent},
+  {path: 'edit/:id', component: PersonAddComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
